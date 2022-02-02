df9621d0000a936ea7a741effe974486d7dd7af1
bhasi <anilbhasib@gmail.com>
2022-02-02 22:17:02 +0530

    Bp

---
+++ src/pages/ArticlesList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ArticlesList() {
+return <div>Article Lis</div>;
+}
+
+export default ArticlesList;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home() {
+  return <div className='home'>
+      <h1>My Blog Application</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint provident autem dolorum, magnam temporibus laborum quibusdam officiis? Enim, unde nemo?</p>
+  </div>;
+}
+
+export default Home;
