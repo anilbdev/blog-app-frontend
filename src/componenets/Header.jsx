2f6fd444370518d0ff7ac36950004aea8f694720
bhasi <anilbhasib@gmail.com>
2022-02-03 08:43:59 +0530

    usenaviagte used on header logo

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css'
 
 function Header() {
+    const navigate =  useNavigate()
   return <div>
       <nav className="header">
-          <h2 className="logo">Blogs</h2>
+          <h2 className="logo" onClick={()=>navigate('/')} >Blogs</h2>
           <div className="articles">
               <Link style={{fontSize:20,marginRight:15,textDecoration:'none'}}  to='/'>Home</Link>
-              <Link style={{fontSize:20,marginRight:15,textDecoration:'none'}} to='/about'>About</Link>
               <Link style={{fontSize:20,marginRight:15,textDecoration:'none'}}  to='/articles-list'>Articles</Link>
+              <Link style={{fontSize:20,marginRight:15,textDecoration:'none'}} to='/about'>About</Link>
           </div>
       </nav>
   </div>;
