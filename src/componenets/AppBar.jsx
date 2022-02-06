bedd6ec2a9b74bd4a98c8db48509773b519e4e02
bhasi <anilbhasib@gmail.com>
2022-02-06 11:11:40 +0530

    appbar done

--- src/componenets/AppBar.jsx
+++ src/componenets/AppBar.jsx
@@ -1,48 +1,35 @@
 import * as React from "react"
 import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
 import Toolbar from "@mui/material/Toolbar"
-import IconButton from "@mui/material/IconButton"
 import Typography from "@mui/material/Typography"
-import Menu from "@mui/material/Menu"
-import MenuIcon from "@mui/icons-material/Menu"
-import Container from "@mui/material/Container"
-import Avatar from "@mui/material/Avatar"
 import Button from "@mui/material/Button"
-import Tooltip from "@mui/material/Tooltip"
-import MenuItem from "@mui/material/MenuItem"
+import Container from "@mui/material/Container"
+import AccountMenu from "./AccountMenu"
+import LoginIcon from "@mui/icons-material/Login"
+import { useNavigate } from "react-router-dom"
 
-const pages = ["Products", "Pricing", "Blog"]
-const settings = ["Profile", "Account", "Dashboard", "Logout"]
+
+
 
 const HeaderBar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null)
-  const [anchorElUser, setAnchorElUser] = React.useState(null)
 
-  const handleOpenNavMenu = event => {
-    setAnchorElNav(event.currentTarget)
-  }
-  const handleOpenUserMenu = event => {
-    setAnchorElUser(event.currentTarget)
-  }
+  const [loggedIn, setLoggedIn] = React.useState(false)
+  const navigate = useNavigate()
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
-  }
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null)
-  }
 
   return (
-    <AppBar sx={{ backgroundColor: "#207398" ,height:'70px'}} position="static">
+    <AppBar
+      sx={{ backgroundColor: "#207398", height: "70px" }}
+      position="static"
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ mr: 2, display: { xs: "none", md: "flex" }, flexGrow: 1, }}
+            sx={{ mr: 2, display: { xs: "none", md: "flex" }, flexGrow: 1 }}
           >
             Blogs
           </Typography>
@@ -59,36 +46,25 @@ const HeaderBar = () => {
             Blogs
           </Typography>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">Account</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">Logout</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
+          {loggedIn ? (
+            <AccountMenu/>
+          ) : (
+            <Button
+            variant="contained"
+            sx={{
+              backgroundColor: "#E21717",
+              "&:hover": {
+                backgroundColor: "#f8abab",
+              },
+            }}
+            startIcon={<LoginIcon />}
+            onClick={() => {
+              navigate("/login")
+            }}
+          >
+            Login
+          </Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
