b6082998cc31c84e8ba52b8a06bdf793f366a346
bhasi <anilbhasib@gmail.com>
2022-02-08 07:08:50 +0530

    login input ui done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,6 +7,10 @@ import Box from "@mui/material/Box"
 import Grid from "@mui/material/Grid"
 import Paper from "@mui/material/Paper"
 import { calculateNewValue } from "@testing-library/user-event/dist/utils"
+import TextField from "@mui/material/TextField"
+import LoginIcon from "@mui/icons-material/Login"
+import Button from "@mui/material/Button"
+import { borderRadius } from "@mui/system"
 
 function Login() {
   const [formValues, setFormValues] = useState({
@@ -25,7 +29,7 @@ function Login() {
     setFormValues({ ...formValues, [name]: value })
   }
 
-  //!validation and sending details to server
+  //!validation and sending details to validation-client side
   const handleSubmit = event => {
     event.preventDefault()
     setFormErrorValues(validation_login(formValues))
@@ -56,53 +60,73 @@ function Login() {
   }, [isSubmit])
 
   return (
-    <Box sx={{backgroundColor:'#207398'}}>
-    <Container maxWidth="sm" >
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        sx={{
-         
-          height:"calc(100vh - 70px)" 
-        }}
-      >
-      <Grid item  xs={10} md={6}>
-        <Paper elevation={4} >
-        <form className="form-container" onSubmit={handleSubmit}>
-          <h2>Login</h2>
-          <div className="form-inputs">
-            <label htmlFor="email">
-              Email
-              <input
-                name="email"
-                type="text"
-                id="email"
-                value={formValues.email}
-                onChange={handleOnChange}
-              />
-              <p>{formErrorValues.email}</p>
-            </label>
-            <label htmlFor="password">
-              Password
-              <input
-                name="password"
-                type="password"
-                id="password"
-                autoComplete="on"
-                value={formValues.password}
-                onChange={handleOnChange}
-              />
-              <p>{formErrorValues.password}</p>
-            </label>
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        </Paper>
-      </Grid>
-      </Grid>
-    </Container>
+    <Box sx={{ backgroundColor: "#207398" }}>
+      <Container maxWidth="sm">
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            height: "calc(100vh - 70px)",
+          }}
+        >
+          <Grid item xs={10} md={6}>
+            <Paper elevation={4} sx={{ backgroundColor: "#8C8984", borderRadius:5 }}>
+              <form
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: 300,
+                }}
+                className="form-container"
+                onSubmit={handleSubmit}
+              >
+                <h2>Login</h2>
+                <div className="form-inputs">
+                  <TextField
+                    error={formErrorValues.email && true}
+                    id="standard-error-helper-text"
+                    label="Email"
+                    helperText={formErrorValues.email || " "}
+                    variant="standard"
+                    name="email"
+                    onChange={handleOnChange}
+                    value={formValues.email}
+                  />
+
+                  <TextField
+                    error={formErrorValues.password && true}
+                    id="standard-error-helper-text"
+                    label="Password"
+                    helperText={formErrorValues.password || " "}
+                    variant="standard"
+                    name="password"
+                    onChange={handleOnChange}
+                    value={formValues.password}
+                  />
+                </div>
+                {/* <button type="submit">Login</button> */}
+                <Button
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "#E21717",
+                    "&:hover": {
+                      backgroundColor: "#f8abab",
+                    },
+                  }}
+                  startIcon={<LoginIcon />}
+                  type='submit'
+                >
+                  Login
+                </Button>
+              </form>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   )
 }
